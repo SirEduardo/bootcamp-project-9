041cd3bd3da3736cc4eb4ec1f546c2e3e136c421
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-07-02 10:40:41 +0200

    update  index.js

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-const pcArray = []
+const pcArray = [];
 
-const limpiarPrecio = (precio) => {
-  precio = precio.replace(",", ".")
-
-  if (precio.includes("-")) {
-    precio = precio.replace("-", "00")
-  }
-
-  return parseFloat(precio);
-};
 
 const scrapper = async (url) => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: false });
 
-  const page = await browser.newPage()
+  const page = await browser.newPage();
 
-  await page.goto(url)
+  await page.goto(url);
 
-  await page.setViewport({ width: 1080, height: 1024 })
+  await page.setViewport({ width: 1080, height: 1024 });
 
-  const cookieButton = await page.$("#pwa-consent-layer-accept-all-button")
+  const cookieButton = await page.$("#pwa-consent-layer-accept-all-button");
   if (cookieButton) {
-    await cookieButton.click()
+    await cookieButton.click();
   }
 
   await repeat(page);
+  await browser.close();
 };
 
 const repeat = async (page) => {
@@ -36,52 +28,63 @@ const repeat = async (page) => {
   );
 
   const arrayDivs = await page.$$(
-    ".sc-b0a2f165-0.hJZrCI.sc-1ecb3c22-3.kAEzmY.sc-1036526f-2.bmkiWp"
+    '[data-test="mms-search-srp-productlist"] div.sc-b0c0d999-0'
   );
+  if (arrayDivs.length === 0) {
+    await paginate(page);
+    return;
+  }
 
   for (const pcDivs of arrayDivs) {
     try {
       let price = await pcDivs.$eval(
-        ".sc-f524209-0.eDhLj span",
+        ".sc-f524209-0.eDhLj span.sc-3f2da4f5-0.sc-dd1a61d2-2.efAprc",
         (el) => el.textContent.trim()
-      )
-      price = limpiarPrecio(price); 
+      );
 
-      let title = await pcDivs.$eval(
-        "p.sc-3f2da4f5-0.fLePRG",
-        (el) => el.textContent.trim()
-      )
+      let title = await pcDivs.$eval("p.sc-3f2da4f5-0.fLePRG", (el) =>
+        el.textContent.trim()
+      );
 
-      let img = await pcDivs.$eval("img", (el) => el.src)
+      let img = await pcDivs.$eval("img", (el) => el.src);
 
       const pc = {
         img,
         title,
         price,
-      }
-      pcArray.push(pc)
+      };
+      pcArray.push(pc);
     } catch (error) {
-      console.error(`Error procesando un div: ${error}`)
+      console.error(`Error procesando un div: ${error}`);
     }
   }
+  await paginate(page);
+};
 
-  try {
-    const nextPageButton = await page.$(".sc-ad462765-1.gxDSlO")
-    if (nextPageButton) {
-      await nextPageButton.click()
-      await page.waitForNavigation()
-      await repeat(page)
-    } else {
-      write(pcArray)
+
+const paginate = async (page) => {
+  const nextPageButton = await page.$('[data-test="mms-search-srp-loadmore"]');
+  if (nextPageButton) {
+    try {
+      await nextPageButton.click();
+      await page.waitForNavigation();
+      await repeat(page);
+    } catch (error) {
+      console.error(`Error en la paginación: ${error}`);
+      write(pcArray);
     }
-  } catch (error) {
-    console.error(`Error en la paginación: ${error}`)
-    write(pcArray)
+  } else {
+    write(pcArray);
   }
 };
+
 const write = (pcArray) => {
-  fs.writeFile("pc.json", JSON.stringify(pcArray), () => {
-    console.log("Archivo escrito");
+  fs.writeFile("pc.json", JSON.stringify(pcArray, null, 2), (err) => {
+    if (err) {
+      console.log("Error escribiendo archivo", err);
+    } else {
+      console.log("Archivo escrito");
+    }
   });
 };
 
